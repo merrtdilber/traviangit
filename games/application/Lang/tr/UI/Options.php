ac08ee43a19e7fe5b535db44694e75760c63ec6b
merrtdilber <mertdlbr@gmail.com>
2025-04-26 21:46:58 +0300

    Update Options.php

--- games/application/Lang/tr/UI/Options.php
+++ games/application/Lang/tr/UI/Options.php
@@ -1,8 +1,8 @@
 <?php 
 $lang['Options'] = array(
-    'TZ01' => 'Time zone preferences',
-    'TZ02' => 'You can change your time zone here.',
-    'TZ03' => 'Time zone',
-    'TZ04' => 'Date format',
+    'TZ01' => 'Saat dilimi tercihleri',
+    'TZ02' => 'Saat diliminizi burada değiştirebilirsiniz.',
+    'TZ03' => 'Saat dilimi',
+    'TZ04' => 'Tarih formatı',
 );
-
+?>
